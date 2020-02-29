47bf0f61940f5ca43b946e309b5f76af85981d69
sssylvan <ssylvan@foxmail.com>
2020-02-29 23:49:48 +0800

    集成 企业微信消息

--- api/hello.ts
+++ api/hello.ts
@@ -1,6 +1,25 @@
 import { NowRequest, NowResponse } from '@now/node'
+const rp = require('request-promise');
 
-export default (request: NowRequest, response: NowResponse) => {
-  const { name = 'World' } = request.query
-  response.status(200).send(`TS, Hello ${name}!`)
+const workWeChatHooks = 'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=111caaee-4e34-48c6-b91b-df2962995167'
+
+export default (_: NowRequest, response: NowResponse) => {
+
+  const options = {
+    method:"POST",
+    uri:workWeChatHooks,
+   body: {
+      "msgtype": "markdown",
+      "markdown": {
+          "content": `实时新增用户反馈<font color=\"warning\">132例</font>，请相关同事注意。\n
+           >类型:<font color=\"comment\">用户反馈</font>
+           >普通用户反馈:<font color=\"comment\">117例</font>
+           >VIP用户反馈:<font color=\"comment\">15例</font>`
+      }
+  },
+  json:true
+  
+  }
+  rp(options).then(rst=>response.json(rst))
 }
+
