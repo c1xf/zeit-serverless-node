aef4c9efad7e648c637593430eedc7362a13aba9
sssylvan <ssylvan@foxmail.com>
2020-02-29 23:34:38 +0800

    distinguish  ts and js

--- api/hello.ts
+++ api/hello.ts
@@ -2,5 +2,5 @@ import { NowRequest, NowResponse } from '@now/node'
 
 export default (request: NowRequest, response: NowResponse) => {
   const { name = 'World' } = request.query
-  response.status(200).send(`Hello ${name}!`)
+  response.status(200).send(`TS, Hello ${name}!`)
 }
