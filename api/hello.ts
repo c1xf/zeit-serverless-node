0c462f2038c0d9d36eeed9a9f2d805a8aee04f37
sssylvan <ssylvan@foxmail.com>
2020-03-01 10:27:10 +0800

    update wechat msg

--- api/hello.ts
+++ api/hello.ts
@@ -5,14 +5,15 @@ const workWeChatHooks =
   "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=111caaee-4e34-48c6-b91b-df2962995167";
 
 export default (request: NowRequest, response: NowResponse) => {
-  // const data = request.body
+  const data = request.body
   const options = {
     method: "POST",
     uri: workWeChatHooks,
     body: {
       msgtype: "markdown",
       markdown: {
-        content: `hello sentry ${new Date()}`
+        content: `hello sentry ${new Date()}
+        [查看详情](${data.url})`
       }
     },
     json: true
