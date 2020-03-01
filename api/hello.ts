070f49a006a07597d80da8e9e8188a0829da0208
sssylvan <ssylvan@foxmail.com>
2020-03-01 10:00:12 +0800

    Integrate sentry

--- api/hello.ts
+++ api/hello.ts
@@ -1,25 +1,21 @@
-import { NowRequest, NowResponse } from '@now/node'
-const rp = require('request-promise');
+import { NowRequest, NowResponse } from "@now/node";
+const rp = require("request-promise");
 
-const workWeChatHooks = 'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=111caaee-4e34-48c6-b91b-df2962995167'
+const workWeChatHooks =
+  "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=111caaee-4e34-48c6-b91b-df2962995167";
 
-export default (_: NowRequest, response: NowResponse) => {
-
+export default (request: NowRequest, response: NowResponse) => {
+  const data = request.body
   const options = {
-    method:"POST",
-    uri:workWeChatHooks,
-   body: {
-      "msgtype": "markdown",
-      "markdown": {
-          "content": `实时新增用户反馈<font color=\"warning\">132例</font>，请相关同事注意。
-           类型:<font color=\"comment\">用户反馈</font>
-           普通用户反馈:<font color=\"comment\">117例</font>
-           VIP用户反馈:<font color=\"comment\">15例</font>`
+    method: "POST",
+    uri: workWeChatHooks,
+    body: {
+      msgtype: "markdown",
+      markdown: {
+        content: `${JSON.stringify(data)}`
       }
-  },
-  json:true
-  
-  }
-  rp(options).then(rst=>response.json(rst))
-}
-
+    },
+    json: true
+  };
+  rp(options).then(rst => response.json(rst));
+};
