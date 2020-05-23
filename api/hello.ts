7e01116ab7c4874207a9538a14191a8261c62551
sssylvan <ssylvan@foxmail.com>
2020-05-23 15:34:56 +0800

    fix build error

--- api/hello.ts
+++ api/hello.ts
@@ -1,26 +1,26 @@
-import { NowRequest, NowResponse } from "@now/node";
-const rp = require("request-promise");
+import { NowRequest, NowResponse } from '@now/node'
+const rp = require('request-promise')
 
 const workWeChatHooks =
-  "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=181322e6-9f07-43dc-add2-2db5aed6281b";
+  'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=181322e6-9f07-43dc-add2-2db5aed6281b'
 
 export default (request: NowRequest, response: NowResponse) => {
   const data = request.body
   console.log(data)
-  const {project,url,event}={...data}
+  const { project = '', url = '', event = null } = { ...data }
   const options = {
-    method: "POST",
+    method: 'POST',
     uri: workWeChatHooks,
     body: {
-      msgtype: "markdown",
+      msgtype: 'markdown',
       markdown: {
         content: `${event?.title}
          Project：${project}
          url：${url}
-         origin：${event?.request.url}`
-      }
+         origin：${event?.request.url}`,
+      },
     },
-    json: true
-  };
-  rp(options).then(_ => response.status(200).send(data));
-};
+    json: true,
+  }
+  rp(options).then((_) => response.status(200).send(data))
+}
